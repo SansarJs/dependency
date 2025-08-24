ba2431415df8c69c7ebdaa301bed1a3abe738c8f
SalathielGenese <salathielgenese@gmail.com>
2025-08-24 19:29:02 +0200

    feat: test registration of key/value and retrieval afterward
    Signed-off-by: SalathielGenese <salathielgenese@gmail.com>

--- index.ts
+++ index.ts
@@ -1,3 +1,30 @@
+export class Container {
+  readonly #values = new WeakMap<Class, unknown>();
+
+  get<T>(key: Class<Token<T>>): T;
+  get<T>(key: Class<T>): T;
+  get<T>(key: Class<Token<T>> | Class<T>): T {
+    throw -1;
+  }
+
+  register<T>(key: Class<Token<T>>, provider: { value: T }): this;
+  register<T>(key: Class<T>, provider: { value: T }): this;
+  register(key: Class, provider: { value: unknown }): this {
+    return this;
+  }
+}
+
+export abstract class ContainerError extends Error {}
+export class ContainerDuplicateKeyError extends ContainerError {
+  constructor(key: Class, options?: { cause?: unknown }) {
+    super(`Duplicate key registration error: ${key.name}`, options);
+  }
+}
+
+type Class<T = unknown, A extends unknown[] = unknown[]> = {
+  new (...args: A): T;
+};
+
 const token = Symbol();
 export class Token<T = unknown> {
   readonly [token]: T | undefined;
--- index_test.ts
+++ index_test.ts
@@ -1,6 +1,47 @@
 import { describe, it } from "@std/testing/bdd";
 import { expect } from "@std/expect";
-import { Token } from "./index.ts";
+
+import { Container, ContainerDuplicateKeyError, Token } from "./index.ts";
+
+describe("Container", () => {
+  describe("register(key, { value })", () => {
+    it("will return the container object", () => {
+      class A {}
+      class AToken extends Token<A> {}
+      const container = new Container();
+
+      expect(container.register(A, { value: new A() })).toBe(container);
+      expect(container.register(AToken, { value: new A() })).toBe(container);
+    });
+
+    it("will cause Container#get to return the same value for the same token", () => {
+      class A {}
+      class AToken extends Token<A> {}
+      const [one, two] = [new A(), new A()];
+      const container = new Container()
+        .register(A, { value: one })
+        .register(AToken, { value: two });
+
+      expect(container.get(A)).toBe(one);
+      expect(container.get(AToken)).toBe(two);
+    });
+
+    it("will throw on a second key registration", () => {
+      class A {}
+      class AToken extends Token<A> {}
+      const container = new Container()
+        .register(A, { value: new A() })
+        .register(AToken, { value: new A() });
+
+      expect(() => container.register(A, { value: new A() })).toThrow(
+        ContainerDuplicateKeyError,
+      );
+      expect(() => container.register(AToken, { value: new A() })).toThrow(
+        ContainerDuplicateKeyError,
+      );
+    });
+  });
+});
 
 describe("Token", () => {
   it("cannot instantiate base class", () => {
