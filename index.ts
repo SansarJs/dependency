b320714aeef0eca3bbd0a4f826c26511888861b5
SalathielGenese <salathielgenese@gmail.com>
2025-08-24 21:23:21 +0200

    feat: test registration of key/resolver and retrieval afterward
    Signed-off-by: SalathielGenese <salathielgenese@gmail.com>

--- index.ts
+++ index.ts
@@ -1,21 +1,21 @@
 export class Container {
   readonly #values = new WeakMap<Class, unknown>();
 
-  get<T>(key: Class<Token<T>>): T;
-  get<T>(key: Class<T>): T;
   get<T>(key: Class<Token<T>> | Class<T>): T {
     if (this.#values.has(key)) return this.#values.get(key) as T;
     throw -1;
   }
 
-  register<T>(key: Class<Token<T>>, provider: { value: T }): this;
-  register<T>(key: Class<T>, provider: { value: T }): this;
-  register(key: Class, provider: { value: unknown }): this {
+  register<T>(key: Class<Token<T>>, provider: Provider<T>): this;
+  register<T>(key: Class<T>, provider: Provider<T>): this;
+  register(key: Class, provider: Provider): this {
     if (this.#values.has(key)) throw new ContainerDuplicateKeyError(key);
-    this.#values.set(key, provider.value);
+    if ("value" in provider) this.#values.set(key, provider.value);
     return this;
   }
 }
+
+export type Provider<T = unknown> = { resolver: () => T } | { value: T };
 
 export abstract class ContainerError extends Error {}
 export class ContainerDuplicateKeyError extends ContainerError {
--- index_test.ts
+++ index_test.ts
@@ -1,9 +1,58 @@
 import { describe, it } from "@std/testing/bdd";
-import { expect } from "@std/expect";
+import { expect, fn } from "@std/expect";
 
 import { Container, ContainerDuplicateKeyError, Token } from "./index.ts";
 
 describe("Container", () => {
+  describe("get(key)", () => {
+    it("invoke related resolver definition on first time", () => {
+      class A {}
+      class AToken extends Token<A> {}
+      const [aMock, aTokenMock] = [fn(), fn()];
+      const container = new Container()
+        .register(A, { resolver: aMock as () => A })
+        .register(AToken, { resolver: aTokenMock as () => A });
+
+      expect(aMock).not.toBeCalled();
+      expect(aTokenMock).not.toBeCalled();
+
+      container.get(A);
+      container.get(AToken);
+      expect(aMock).toBeCalledTimes(1);
+      expect(aTokenMock).toBeCalledTimes(1);
+    });
+
+    it("invoke related resolver definition only first time", () => {
+      class A {}
+      class AToken extends Token<A> {}
+      const [aMock, aTokenMock] = [fn(), fn()];
+      const container = new Container()
+        .register(A, { resolver: aMock as () => A })
+        .register(AToken, { resolver: aTokenMock as () => A });
+
+      container.get(A);
+      container.get(AToken);
+      expect(aMock).toBeCalledTimes(1);
+      expect(aTokenMock).toBeCalledTimes(1);
+
+      container.get(A);
+      container.get(AToken);
+      expect(aMock).toBeCalledTimes(1);
+      expect(aTokenMock).toBeCalledTimes(1);
+    });
+
+    it("cache the first resolver returned value", () => {
+      class A {}
+      class AToken extends Token<A> {}
+      const container = new Container()
+        .register(A, { resolver: () => new A() })
+        .register(AToken, { resolver: () => new A() });
+
+      expect(container.get(A)).toBe(container.get(A));
+      expect(container.get(AToken)).toBe(container.get(AToken));
+    });
+  });
+
   describe("register(key, { value })", () => {
     it("will return the container object", () => {
       class A {}
@@ -26,7 +75,7 @@ describe("Container", () => {
       expect(container.get(AToken)).toBe(two);
     });
 
-    it("will throw on a second key registration", () => {
+    it("will throw on same key registration", () => {
       class A {}
       class AToken extends Token<A> {}
       const container = new Container()
@@ -39,6 +88,48 @@ describe("Container", () => {
       expect(() => container.register(AToken, { value: new A() })).toThrow(
         ContainerDuplicateKeyError,
       );
+    });
+  });
+
+  describe("register(key, { resolver })", () => {
+    it("will return the container object", () => {
+      class A {}
+      class AToken extends Token<A> {}
+      const container = new Container();
+
+      expect(container.register(A, { resolver: () => new A() })).toBe(
+        container,
+      );
+      expect(container.register(AToken, { resolver: () => new A() })).toBe(
+        container,
+      );
+    });
+
+    it("will cause Container#get to return the factory returned value for the same token", () => {
+      class A {}
+      class AToken extends Token<A> {}
+      const [one, two] = [new A(), new A()];
+      const container = new Container()
+        .register(A, { resolver: () => one })
+        .register(AToken, { resolver: () => two });
+
+      expect(container.get(A)).toBe(one);
+      expect(container.get(AToken)).toBe(two);
+    });
+
+    it("will throw on same key registration", () => {
+      class A {}
+      class AToken extends Token<A> {}
+      const container = new Container()
+        .register(A, { resolver: () => new A() })
+        .register(AToken, { resolver: () => new A() });
+
+      expect(() => container.register(A, { resolver: () => new A() })).toThrow(
+        ContainerDuplicateKeyError,
+      );
+      expect(() =>
+        container.register(AToken, { resolver: () => new A() }),
+      ).toThrow(ContainerDuplicateKeyError);
     });
   });
 });
